cf98e9085105d6a8c96220aec1eaeabceca5b717
Yubing Hou <houyubing24@hotmail.com>
2018-07-21 09:56:53 -0500

    updated build scripts and documentation

--- go.test.sh
+++ go.test.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
-#
+
 # test script for codecov
+#
+# Troubleshoot for Windows:
+# This program may not be able to run on Windows bash since Windows uses a different file-encoding from ASCII
+# To remove non-ASCII characters from this script, run:
+# $ hexdump -C go.test.sh
+# Look for 0d 0a sqeuences. We can strip thie \r (0d) with the tr command:
+# $ cat go.test.sh | tr -d '\r' >> go.test2.sh
+# Then copy the content from go.test2.sh to go.test.sh, and remove go.test2.sh
+# This solution was from Stackoverflow:
+# https://stackoverflow.com/questions/5491634/shell-script-error-expecting-do
 set -e
 echo "" > coverage.txt
 #

7547c119cc0ca21d90d9015c24c946e4a82b9dec
shortaflip <kevin.enario.354@my.csun.edu>
2019-01-24 16:19:17 -0800

    #10: added tests for all working error and success paths for competition.go/UpdateCompetitions 1/24/2019

--- businesslogic/competition_test.go
+++ businesslogic/competition_test.go
@@ -1,5 +1,5 @@
 // Dancesport Application System (DAS)
-// Copyright (C) 2017, 2018 Yubing Hou
+// Copyright (C) 2017, time.Now().Year() Yubing Hou
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -81,9 +81,9 @@ func TestCompetition_UpdateStatus(t *testing.T) {
 
 func TestCompetition_GetStatus(t *testing.T) {
 	comp := businesslogic.Competition{}
-	comp.UpdateStatus(businesslogic.CompetitionStatusCancelled)
+	comp.UpdateStatus(businesslogic.CompetitionStatusClosedRegistration)
 
-	assert.Equal(t, businesslogic.CompetitionStatusCancelled, comp.GetStatus())
+	assert.Equal(t, businesslogic.CompetitionStatusClosedRegistration, comp.GetStatus())
 }
 
 // GetCompetitionByID test helpers
@@ -160,25 +160,254 @@ func TestCompetition_GetCompetitionByID_Success(t *testing.T) {
 	getCompetitionByIDAssertNilHandler(t, competitionRepo)
 }
 
+// UpdateCompetition test helper functions
+func updateCompetitionMockHandler(m *gomock.Controller, userID int, compID int, status int,
+	name string, website string, startDate time.Time, endDate time.Time,
+	comp []businesslogic.Competition, err error) (businesslogic.Account,
+	businesslogic.OrganizerUpdateCompetition, *mock_businesslogic.MockICompetitionRepository) {
+
+	user := businesslogic.Account{ID: userID}
+	competition := businesslogic.OrganizerUpdateCompetition{
+		CompetitionID: compID,
+		Status:        status,
+		Name:          name,
+		Website:       website,
+		StartDate:     startDate,
+		EndDate:       endDate,
+	}
+	searchComp := businesslogic.SearchCompetitionCriteria{ID: competition.CompetitionID}
+	competitionRepo := mock_businesslogic.NewMockICompetitionRepository(m)
+	competitionRepo.EXPECT().SearchCompetition(searchComp).Return(comp, err)
+
+	return user, competition, competitionRepo
+}
+
+// UpdateCompetition tests
 func TestCompetition_UpdateCompetition_ErrorNotNil(t *testing.T) {
 	mockCtrl := gomock.NewController(t)
 	defer mockCtrl.Finish()
 
-	user := businesslogic.Account{ID: 2}
-	competition := businesslogic.OrganizerUpdateCompetition{
-		CompetitionID: 2,
-		Status:        businesslogic.CompetitionStatusInProgress,
-		Name:          "The Great American Ball",
-		Website:       "www.tgab.com",
-		StartDate:     time.Date(2018, time.November, 18, 9, 0, 0, 0, time.UTC),
-		EndDate:       time.Date(2018, time.November, 19, 22, 0, 0, 0, time.UTC),
-	}
-	searchComp := businesslogic.SearchCompetitionCriteria{ID: competition.CompetitionID}
-	competitionRepo := mock_businesslogic.NewMockICompetitionRepository(mockCtrl)
-	competitionRepo.EXPECT().SearchCompetition(searchComp).Return(
-		[]businesslogic.Competition{},
-		errors.New("Return an error"),
-	)
+	// test parameters
+	start := time.Date(time.Now().Year(), time.Now().AddDate(0, 6, 0).Month(), 18, 9, 0, 0, 0, time.UTC)
+	end := time.Date(time.Now().Year(), time.Now().AddDate(0, 6, 0).Month(), 19, 22, 0, 0, 0, time.UTC)
+
+	// initialize mocks
+	user, competition, competitionRepo := updateCompetitionMockHandler(mockCtrl, 2, 2,
+		businesslogic.CompetitionStatusInProgress, "The Great American Ball", "www.tgab.com",
+		start, end, []businesslogic.Competition{}, errors.New("Return an error"))
 
 	assert.Error(t, businesslogic.UpdateCompetition(&user, competition, competitionRepo))
+}
+
+func TestCompetition_UpdateCompetition_SearchResultNil(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	// test parameters
+	start := time.Date(time.Now().Year(), time.Now().AddDate(0, 6, 0).Month(), 18, 9, 0, 0, 0, time.UTC)
+	end := time.Date(time.Now().Year(), time.Now().AddDate(0, 6, 0).Month(), 19, 22, 0, 0, 0, time.UTC)
+
+	// initialize mocks
+	user, competition, competitionRepo := updateCompetitionMockHandler(mockCtrl, 2, 2,
+		businesslogic.CompetitionStatusInProgress, "The Great American Ball", "www.tgab.com",
+		start, end, nil, errors.New("cannot find this competition"))
+
+	assert.Error(t, businesslogic.UpdateCompetition(&user, competition, competitionRepo))
+}
+
+func TestCompetition_UpdateCompetition_SearchResultNotOne(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	// test parameters
+	start := time.Date(time.Now().Year(), time.Now().AddDate(0, 6, 0).Month(), 18, 9, 0, 0, 0, time.UTC)
+	end := time.Date(time.Now().Year(), time.Now().AddDate(0, 6, 0).Month(), 19, 22, 0, 0, 0, time.UTC)
+
+	// initialize mocks
+	user, competition, competitionRepo := updateCompetitionMockHandler(mockCtrl, 2, 2,
+		businesslogic.CompetitionStatusInProgress, "The Great American Ball", "www.tgab.com",
+		start, end, make([]businesslogic.Competition, 2), errors.New("cannot find this competition"))
+
+	assert.Error(t, businesslogic.UpdateCompetition(&user, competition, competitionRepo))
+}
+
+func TestCompetition_UpdateCompetition_CompIDEqualsZero(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	// test parameters
+	start := time.Date(time.Now().Year(), time.Now().AddDate(0, 6, 0).Month(), 18, 9, 0, 0, 0, time.UTC)
+	end := time.Date(time.Now().Year(), time.Now().AddDate(0, 6, 0).Month(), 19, 22, 0, 0, 0, time.UTC)
+	comp := []businesslogic.Competition{{ID: 0}}
+
+	// initialize mocks
+	user, competition, competitionRepo := updateCompetitionMockHandler(mockCtrl, 2, 2,
+		businesslogic.CompetitionStatusInProgress, "The Great American Ball", "www.tgab.com",
+		start, end, comp, errors.New("cannot find this competition"))
+
+	assert.Error(t, businesslogic.UpdateCompetition(&user, competition, competitionRepo))
+}
+
+func TestCompetition_UpdateCompetition_ValidateUpdateCompetition_CreateUserIDNoMatch(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	// test parameters
+	start := time.Date(time.Now().Year(), time.Now().AddDate(0, 6, 0).Month(), 18, 9, 0, 0, 0, time.UTC)
+	end := time.Date(time.Now().Year(), time.Now().AddDate(0, 6, 0).Month(), 19, 22, 0, 0, 0, time.UTC)
+	comp := []businesslogic.Competition{{ID: 2, CreateUserID: 3}}
+
+	// initialize mocks
+	user, competition, competitionRepo := updateCompetitionMockHandler(mockCtrl, 2, 2,
+		businesslogic.CompetitionStatusInProgress, "The Great American Ball", "www.tgab.com",
+		start, end, comp, nil)
+
+	assert.Error(t, businesslogic.UpdateCompetition(&user, competition, competitionRepo))
+}
+
+func TestCompetition_UpdateCompetition_ValidateUpdateCompetition_UpdateStatusInvalid(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	// test parameters
+	start := time.Date(time.Now().Year(), time.Now().AddDate(0, 6, 0).Month(), 18, 9, 0, 0, 0, time.UTC)
+	end := time.Date(time.Now().Year(), time.Now().AddDate(0, 6, 0).Month(), 19, 22, 0, 0, 0, time.UTC)
+	comp := []businesslogic.Competition{{ID: 2, CreateUserID: 2}}
+	comp[0].UpdateStatus(businesslogic.CompetitionStatusProcessing)
+
+	// initialize mocks
+	user, competition, competitionRepo := updateCompetitionMockHandler(mockCtrl, 2, 2,
+		businesslogic.CompetitionStatusInProgress, "The Great American Ball", "www.tgab.com",
+		start, end, comp, nil)
+
+	assert.Error(t, businesslogic.UpdateCompetition(&user, competition, competitionRepo))
+}
+
+func TestCompetition_UpdateCompetition_ValidateUpdateCompetition_StatusClosed(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	// test parameters
+	start := time.Date(time.Now().Year(), time.Now().AddDate(0, 6, 0).Month(), 18, 9, 0, 0, 0, time.UTC)
+	end := time.Date(time.Now().Year(), time.Now().AddDate(0, 6, 0).Month(), 19, 22, 0, 0, 0, time.UTC)
+	comp := []businesslogic.Competition{{ID: 2, CreateUserID: 2}}
+	comp[0].UpdateStatus(businesslogic.CompetitionStatusClosed)
+
+	// initialize mocks
+	user, competition, competitionRepo := updateCompetitionMockHandler(mockCtrl, 2, 2,
+		businesslogic.CompetitionStatusCancelled, "The Great American Ball", "www.tgab.com",
+		start, end, comp, nil)
+
+	assert.Error(t, businesslogic.UpdateCompetition(&user, competition, competitionRepo))
+}
+
+func TestCompetition_UpdateCompetition_ValidateUpdateCompetition_NameTooShort(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	// test parameters
+	start := time.Date(time.Now().Year(), time.Now().AddDate(0, 6, 0).Month(), 18, 9, 0, 0, 0, time.UTC)
+	end := time.Date(time.Now().Year(), time.Now().AddDate(0, 6, 0).Month(), 19, 22, 0, 0, 0, time.UTC)
+	comp := []businesslogic.Competition{{ID: 2, CreateUserID: 2}}
+	comp[0].UpdateStatus(businesslogic.CompetitionStatusClosedRegistration)
+
+	// initialize mocks
+	user, competition, competitionRepo := updateCompetitionMockHandler(mockCtrl, 2, 2,
+		businesslogic.CompetitionStatusInProgress, "", "www.tgab.com",
+		start, end, comp, nil)
+
+	assert.Error(t, businesslogic.UpdateCompetition(&user, competition, competitionRepo))
+}
+
+func TestCompetition_UpdateCompetition_ValidateUpdateCompetition_WebAddressInvalid(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	// test parameters
+	start := time.Date(time.Now().Year(), time.Now().AddDate(0, 6, 0).Month(), 18, 9, 0, 0, 0, time.UTC)
+	end := time.Date(time.Now().Year(), time.Now().AddDate(0, 6, 0).Month(), 19, 22, 0, 0, 0, time.UTC)
+	comp := []businesslogic.Competition{{ID: 2, CreateUserID: 2}}
+	comp[0].UpdateStatus(businesslogic.CompetitionStatusClosedRegistration)
+
+	// initialize mocks
+	user, competition, competitionRepo := updateCompetitionMockHandler(mockCtrl, 2, 2,
+		businesslogic.CompetitionStatusInProgress, "The Great American Ball", "",
+		start, end, comp, nil)
+
+	assert.Error(t, businesslogic.UpdateCompetition(&user, competition, competitionRepo))
+}
+
+func TestCompetition_UpdateCompetition_ValidateUpdateCompetition_StartDateAfterEndDate(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	// test parameters
+	start := time.Date(time.Now().Year(), time.Now().AddDate(0, 6, 0).Month(), 20, 9, 0, 0, 0, time.UTC)
+	end := time.Date(time.Now().Year(), time.Now().AddDate(0, 6, 0).Month(), 19, 22, 0, 0, 0, time.UTC)
+	comp := []businesslogic.Competition{{ID: 2, CreateUserID: 2}}
+	comp[0].UpdateStatus(businesslogic.CompetitionStatusClosedRegistration)
+
+	// initialize mocks
+	user, competition, competitionRepo := updateCompetitionMockHandler(mockCtrl, 2, 2,
+		businesslogic.CompetitionStatusInProgress, "The Great American Ball", "www.tgab.com",
+		start, end, comp, nil)
+
+	assert.Error(t, businesslogic.UpdateCompetition(&user, competition, competitionRepo))
+}
+
+func TestCompetition_UpdateCompetition_ValidateUpdateCompetition_StartDatePassed(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	// test parameters
+	start := time.Date(time.Now().AddDate(-1, 0, 0).Year(), time.Now().AddDate(0, 6, 0).Month(), 18, 9, 0, 0, 0, time.UTC)
+	end := time.Date(time.Now().Year(), time.Now().AddDate(0, 6, 0).Month(), 19, 22, 0, 0, 0, time.UTC)
+	comp := []businesslogic.Competition{{ID: 2, CreateUserID: 2}}
+	comp[0].UpdateStatus(businesslogic.CompetitionStatusClosedRegistration)
+
+	// initialize mocks
+	user, competition, competitionRepo := updateCompetitionMockHandler(mockCtrl, 2, 2,
+		businesslogic.CompetitionStatusInProgress, "The Great American Ball", "www.tgab.com",
+		start, end, comp, nil)
+
+	assert.Error(t, businesslogic.UpdateCompetition(&user, competition, competitionRepo))
+}
+
+func TestCompetition_UpdateCompetition_ValidateUpdateCompetition_StartDateYearLater(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	// test parameters
+	start := time.Date(time.Now().AddDate(1, 0, 0).Year(), time.Now().AddDate(0, 6, 0).Month(), 18, 9, 0, 0, 0, time.UTC)
+	end := time.Date(time.Now().AddDate(1, 0, 0).Year(), time.Now().AddDate(0, 6, 0).Month(), 19, 22, 0, 0, 0, time.UTC)
+	comp := []businesslogic.Competition{{ID: 2, CreateUserID: 2}}
+	comp[0].UpdateStatus(businesslogic.CompetitionStatusClosedRegistration)
+
+	// initialize mocks
+	user, competition, competitionRepo := updateCompetitionMockHandler(mockCtrl, 2, 2,
+		businesslogic.CompetitionStatusInProgress, "The Great American Ball", "www.tgab.com",
+		start, end, comp, nil)
+
+	assert.Error(t, businesslogic.UpdateCompetition(&user, competition, competitionRepo))
+}
+
+func TestCompetition_UpdateCompetition_Success(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	// test parameters
+	start := time.Date(time.Now().Year(), time.Now().AddDate(0, 6, 0).Month(), 18, 9, 0, 0, 0, time.UTC)
+	end := time.Date(time.Now().Year(), time.Now().AddDate(0, 6, 0).Month(), 19, 22, 0, 0, 0, time.UTC)
+	comp := []businesslogic.Competition{{ID: 2, CreateUserID: 2}}
+	comp[0].UpdateStatus(businesslogic.CompetitionStatusClosedRegistration)
+
+	// initialize mocks
+	user, competition, competitionRepo := updateCompetitionMockHandler(mockCtrl, 2, 2,
+		businesslogic.CompetitionStatusInProgress, "The Great American Ball", "www.tgab.com",
+		start, end, comp, nil)
+
+	// add expected return for success
+	competitionRepo.EXPECT().UpdateCompetition(comp[0]).Return(nil)
+
+	assert.Nil(t, businesslogic.UpdateCompetition(&user, competition, competitionRepo))
 }
